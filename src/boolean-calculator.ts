7356e26fa4335f40f0c4b01bb1d4814f92e14c91
Tom <thomas.spencer@codurance.com>
2020-11-11 19:06:29 +0000

    Add SimpleOperand and Operation

--- src/boolean-calculator.ts
+++ src/boolean-calculator.ts
@@ -1,6 +1,39 @@
 export const calculate = (input: string): boolean => {
-  if (input === "FALSE"){
-    return false;
+  const operations = input.split(" ");
+  const result = operations.map((o) => o.trim());
+
+  if (operations.length === 1){
+    return new SimpleOperand(result[0]).toBoolean();
+  }else {
+    return new Operation(result[0], new SimpleOperand(result[1])).toBoolean()
   }
-  return true;
 }
+
+class SimpleOperand {
+  private value: string;
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  toBoolean(): boolean {
+    if (this.value !== "TRUE" && this.value !== "FALSE"){
+      throw new Error();
+    }
+    return this.value === "TRUE";
+  }
+};
+
+class Operation {
+  private value: string;
+  private operand: SimpleOperand;
+  constructor(value: string, operand: SimpleOperand) {
+    this.value = value;
+    this.operand = operand;
+  }
+
+  toBoolean(): boolean {
+    if (this.value === "NOT"){
+      return !this.operand.toBoolean();
+    }
+  }
+};
--- test/boolean-calculator.test.ts
+++ test/boolean-calculator.test.ts
@@ -8,4 +8,8 @@ describe("Boolean Calculator", () => {
   it("returns false for FALSE string", () => {
     expect(calculate("FALSE")).toEqual(false);
   })
+
+  it("returns false for NOT TRUE string", () => {
+    expect(calculate("NOT TRUE")).toEqual(false);
+  })
 })
