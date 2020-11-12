9cf9ae4ff2ad8acf6153de7d8c1e774cc86c8bca
Tom <thomas.spencer@codurance.com>
2020-11-12 21:34:06 +0000

    Simplify boolean calculator

--- test/boolean-calculator.test.ts
+++ test/boolean-calculator.test.ts
@@ -1,18 +1,6 @@
-import {calculate, parseBoolean} from "../src/boolean-calculator";
+import { parseBoolean } from "../src/boolean-calculator";
 
 describe("Boolean Calculator", () => {
-  it("returns true for TRUE string", () => {
-    expect(calculate("TRUE")).toEqual(true);
-  });
-
-  it("returns false for FALSE string", () => {
-    expect(calculate("FALSE")).toEqual(false);
-  });
-
-  it("returns false for NOT TRUE string", () => {
-    expect(calculate("NOT TRUE")).toEqual(false);
-  });
-
   it.each([
     ["NOT FALSE", true],
     ["TRUE OR FALSE AND NOT FALSE", true],
