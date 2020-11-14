b0c8fdc5606e16a54162b4b8c2841a1e0b74c676
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-11-14 12:41:49 +0000

    Add tests for true not true, false and not false

--- src/boolean-calculator.ts
+++ src/boolean-calculator.ts
@@ -1,57 +1,6 @@
-export const parseBoolean = (expression: string): boolean => {
-  const strings = expression.split(" ");
-  for (let i = 0; i < strings.length; i++) {
-    if (strings[i] === "NOT") {
-      strings[i] = "!";
-    } else if (strings[i] === "AND") {
-      strings[i] = "&";
-    } else if (strings[i] === "OR") {
-      strings[i] = "|";
-    } else if (strings[i] === "TRUE") {
-      strings[i] = "T";
-    } else if (strings[i] === "FALSE") {
-      strings[i] = "F";
-    } else if (strings[i] === "(TRUE") {
-      strings[i] = "(T";
-    } else if (strings[i] === "TRUE)") {
-      strings[i] = "T)";
-    }
+export const parseBoolean = (input: string): boolean => {
+  if (input === "FALSE" || input === "NOT TRUE") {
+    return false;
   }
-
-  const evaluate = strings.join("");
-
-  return helper(evaluate, 0, evaluate.length);
-};
-
-const helper = (expression: string, low: number, high: number): boolean => {
-  if (low == high) {
-    return expression.charAt(low) !== "F";
-  }
-
-  const operator = expression.charAt(low);
-  let count = 0;
-  let result: boolean = operator === "|" ? false : true;
-  let previous: number = low;
-
-  for (let i = low; i <= high; i++) {
-    const c = expression.charAt(i);
-    if (c === "(") {
-      count++;
-    } else if (c === ")") {
-      count--;
-    }
-
-    if (count === 1 || (count === 0 && c === ")")) {
-      const next: boolean = helper(expression, previous, i - 1);
-      previous = i + 1;
-      if (operator === "|") {
-        result = result || next;
-      } else if (operator === "&") {
-        result = result && next;
-      } else {
-        result = !next;
-      }
-    }
-  }
-  return result;
+  return true;
 };
--- test/boolean-calculator.test.ts
+++ test/boolean-calculator.test.ts
@@ -2,12 +2,10 @@ import { parseBoolean } from "../src/boolean-calculator";
 
 describe("Boolean Calculator", () => {
   it.each([
-    // ["NOT FALSE", true],
-    // ["TRUE OR FALSE AND NOT FALSE", true],
-    // ["(TRUE OR TRUE OR TRUE) AND FALSE", false],
-    // ["NOT (TRUE AND TRUE)", false],
-    // ["(TRUE AND FALSE AND TRUE) OR NOT TRUE", false],
-    ["(TRUE AND TRUE) OR NOT TRUE", true],
+    ["TRUE", true],
+    ["NOT TRUE", false],
+    ["FALSE", false],
+    ["NOT FALSE", true],
   ])("returns correct output for input", (input: string, output: boolean) => {
     expect(parseBoolean(input)).toEqual(output);
   });
