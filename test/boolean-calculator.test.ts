ef3c0817d1f0063ca7058b27daebdec86399520b
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-11-14 13:29:00 +0000

    Add extra tests

--- test/boolean-calculator.test.ts
+++ test/boolean-calculator.test.ts
@@ -10,6 +10,14 @@ describe("Boolean Calculator", () => {
     ["TRUE AND FALSE", false],
     ["FALSE AND TRUE", false],
     ["FALSE AND FALSE", false],
+    ["NOT TRUE AND TRUE", false],
+    ["NOT TRUE AND FALSE", false],
+    ["NOT FALSE AND TRUE", true],
+    ["NOT FALSE AND FALSE", false],
+    ["TRUE AND NOT TRUE", false],
+    ["TRUE AND NOT FALSE", true],
+    ["FALSE AND NOT TRUE", false],
+    ["FALSE AND NOT FALSE", false],
   ])("returns correct output for input", (input: string, output: boolean) => {
     expect(parseBoolean(input)).toEqual(output);
   });
