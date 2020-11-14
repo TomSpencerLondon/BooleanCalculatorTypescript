0043b7b7a6370a4be366722d85c1630236c5b1fe
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-11-14 13:12:02 +0000

    Add recursion for NOT operator

--- src/boolean-calculator.ts
+++ src/boolean-calculator.ts
@@ -1,6 +1,25 @@
 export const parseBoolean = (input: string): boolean => {
-  if (input === "FALSE" || input === "NOT TRUE") {
+  if (input === "TRUE") {
+    return true;
+  } else if (input === "FALSE") {
     return false;
   }
-  return true;
+  const array: string[] = input.split(" ");
+
+  if (array[0] === "NOT") {
+    return !parseBoolean(array.slice(1).join(" "));
+  }
+
+  //
+  // const ast: any = {};
+  // const currentBranch: any = ast;
+  //
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] === "AND") {
+  //     currentBranch["AND"] = {
+  //       left: array[i - 1],
+  //       right: array[i + 1],
+  //     };
+  //   }
+  // }
 };
