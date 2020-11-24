aed2de76a5fc4cc8f9deaa62f26a788aeca28073
Tom <thomas.spencer@codurance.com>
2020-11-24 19:13:15 +0000

    Add abstract syntax tree for boolean calculator

--- src/boolean-calculator.ts
+++ src/boolean-calculator.ts
@@ -1,8 +1,10 @@
 export const parseBoolean = (expression: string): boolean => {
-  return parseBooleanHelper(expression);
+  return parseBooleanHelper(expression).value;
 };
 
-const parseBooleanHelper = (expression: string): boolean => {
+const parseBooleanHelper = (
+  expression: string
+): { value: boolean; ast: any } => {
   if (expression.includes("(")) {
     let count = 0;
     for (let i = 0; i < expression.length; i++) {
@@ -14,44 +16,73 @@ const parseBooleanHelper = (expression: string): boolean => {
       }
 
       if (count === 0 && c === ")") {
-        return parseBooleanHelper(
+        const parseBooleanHelperBrackets = parseBooleanHelper(
           expression.substring(0, expression.indexOf("(")) +
             parseBooleanHelper(
               expression.substring(expression.indexOf("(") + 1, i)
-            ) +
+            ).value +
             expression.substring(i + 1, expression.length)
         );
+        return {
+          value: parseBooleanHelperBrackets.value,
+          ast: parseBooleanHelperBrackets.ast,
+        };
       }
     }
   }
   return parseWithoutBrackets(expression.toUpperCase());
 };
 
-const parseWithoutBrackets = (expression: string): boolean => {
+const parseWithoutBrackets = (
+  expression: string
+): { value: boolean; ast: any } => {
   if (expression === "TRUE") {
-    return true;
+    return { value: true, ast: "TRUE" };
   } else if (expression === "FALSE") {
-    return false;
+    return { value: false, ast: "FALSE" };
   }
   const tokens: string[] = expression.split(" ");
 
   if (tokens.length === 2 && tokens[0] === "NOT") {
-    return !parseBooleanHelper(tokens.slice(1).join(" "));
+    const temp: any = parseBooleanHelper(tokens.slice(1).join(" "));
+    return { value: !temp.value, ast: { NOT: temp.ast } };
   }
 
   if (tokens.some((token) => token === "OR")) {
     const index = tokens.indexOf("OR");
-    return (
-      parseBooleanHelper(tokens.slice(0, index).join(" ")) ||
-      parseBooleanHelper(tokens.slice(index + 1).join(" "))
+    const parseBoolean1: any = parseBooleanHelper(
+      tokens.slice(0, index).join(" ")
     );
+    const parseBoolean2: any = parseBooleanHelper(
+      tokens.slice(index + 1).join(" ")
+    );
+    return {
+      value: parseBoolean1.value || parseBoolean2.value,
+      ast: {
+        OR: {
+          left: parseBoolean1.ast,
+          right: parseBoolean2.ast,
+        },
+      },
+    };
   }
 
   if (tokens.some((token) => token === "AND")) {
     const index = tokens.indexOf("AND");
-    return (
-      parseBooleanHelper(tokens.slice(0, index).join(" ")) &&
-      parseBooleanHelper(tokens.slice(index + 1).join(" "))
+    const parseBoolean3: any = parseBooleanHelper(
+      tokens.slice(0, index).join(" ")
     );
+    const parseBoolean4: any = parseBooleanHelper(
+      tokens.slice(index + 1).join(" ")
+    );
+    return {
+      value: parseBoolean3.value && parseBoolean4.value,
+      ast: {
+        AND: {
+          left: parseBoolean3.ast,
+          right: parseBoolean4.ast,
+        },
+      },
+    };
   }
 };
